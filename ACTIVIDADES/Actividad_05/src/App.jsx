ee42b13b822811a1fdc7b728e60aa0d8eac17490
Carlos <cvaquer99@alumnos.cei.es>
2024-02-12 22:29:52 +0100

    update

--- ACTIVIDADES/Actividad_05/src/App.jsx
+++ ACTIVIDADES/Actividad_05/src/App.jsx
@@ -7,12 +7,9 @@ import { Personajes } from './components/Personajes'
 function App() {
 
   // 1. USO DE HOOKS Y DECLARACION DE VARIABLES
+  
+  const [paginaActual, setPaginaActual] = useState("personajes") // primer elemento - variable -- segundo elemento - función que actualiza la variable -- tercer elemento - valor de la variablw inicial
 
-<<<<<<< HEAD
-  const [paginaActual, setPaginaActual] = useState("personajes")
-=======
-  const [paginaActual, setPaginaActual] = useState("") // primer elemento - variable -- segundo elemento - función que actualiza la variable -- tercer elemento - valor de la variablw inicial
->>>>>>> cd5b2c8532fce5fdfa82c4c63cf32364e0ac0931
 
   // 2. DEFINIR FUNCIONES
 
