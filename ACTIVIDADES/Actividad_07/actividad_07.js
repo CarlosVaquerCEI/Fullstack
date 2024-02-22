38622f6d0100f56892735411a6533152fdb02b7c
Carlos <cvaquer99@alumnos.cei.es>
2024-02-22 10:07:38 +0100

    update

--- ACTIVIDADES/Actividad_07/actividad_07.js
+++ ACTIVIDADES/Actividad_07/actividad_07.js
@@ -1,44 +1,107 @@
 import express from "express"
 import datos from "./db/db.js"
 
+const PORT = 8080
+const domain = "http:localhost/"
+
 const app = express()
 console.clear()
 
+const listaLibros = datos.libros
+
+// es el formato que va a tener mi respuesta en todas mis consultas del API
+const responseLibros =  {
+    data: listaLibros,
+    msg:"",
+}
+
+const helperCleanString = (str) => {
+    return str.toString().trim().toLowerCase()
+}
 
 app.get("/libros", (req, res) => {
 
-    console.log(req)
+    responseLibros.data= listaLibros
+    responseLibros.msg="Todos los libros"
+    responseLibros.cant=listaLibros.length
 
-    const jsonData = JSON.stringify(datos)
-    res.setHeader("Content-Type", "application/json")
-    res.send(jsonData)
+    res.status(200).send(responseLibros)
+
 })
 
 app.get("/libros/:id", (req, res) => {
-    const idUsuario = req.params.id
+
+    // dento del req hay un objeto vacio que es params, pero en el momento que se rellene la url se rellenan los valores de params
+
+    const idLibro = req.params.id
+    console.log(req.params)
+
+    // reviso si id es un número
+    if(isNaN(idLibro))  {
+        responseLibros.msg="El id debe ser un libro"
+        delete responseLibros.data
+        res.status(400).send(responseLibros)
+    }
+
+    responseLibros.data= listaLibros.find((libro) => {
+        libro.id==idLibro
+    })
+    responseLibros.msg="Libro con id" + idLibro
     console.log("Libros por id")
-    // res.send("Hola")    
+    res.send(responseLibros)
 
 })
 
 app.get("/libros/author/:author", (req, res) => {
-    const nombreUsuario = req.params.nombre
-    console.log("Libros por nombre de autor")
-    res.send("Hola autor")
+
+    const author=helperCleanString(req.params.author)
+
+    responseLibros.data = listaLibros.filter((libro) => {
+        helperCleanString(libro.autor).includes(author)
+    })
+
+    responseLibros.data= listaLibros
+    responseLibros.msg="Libros por autor"
+    responseLibros.cant=listaLibros.length
+
+    res.status(200).send(responseLibros)
 
 })
 
 app.get("/libros/cathegory/:cathegory", (req, res) => {
-    const categoriaUsuario = req.params.cathegory
-    console.log("Libros por categoría")
-    res.send("Hola categoría")
+    const cate=helperCleanString(req.params.cathegory)
+
+    responseLibros.data = listaLibros.filter((libro) => {
+        helperCleanString(libro.categoria).includes(cate)
+    })
+
+    responseLibros.data= listaLibros
+    responseLibros.msg="Libros por categoría"
+    responseLibros.cant=listaLibros.length
+
+    res.status(200).send(responseLibros)
+
 
 })
 
 app.get("/authors",(req, res) => {
+    responseLibros.data = listaLibros.map((libro) => libro.autor)
+
+
+    // usando forEach
+
+    const uniqueAuthors =[]
+    responseLibros.data.forEach((autor) => {
+        if(!uniqueAuthors.includes(autor)) {
+            uniqueAuthors.push(autor)
+        }
+    })
+
+    responseLibros.data=uniqueAuthors
+
 
 })
 
-app.listen(8080, () => {
-    console.log("Server is running on port 8080")
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
